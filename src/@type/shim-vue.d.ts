c423eb9e041acff272d39e0e13364d7e2ec6ae56
vok123 <512855971@qq.com>
2021-07-06 17:33:17 +0800

    Update shim-vue.d.ts

--- src/@type/shim-vue.d.ts
+++ src/@type/shim-vue.d.ts
@@ -1,30 +1,6 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
 
-export interface VueSlots<T = any> {
-  [name: string]: (payload?: T) => JSX.Element | null;
-}
-
-export declare type VueModel =
-  | string
-  | number
-  | boolean
-  | bigint
-  | null
-  | undefined
-  | symbol
-  | Record<string, unknown>
-  | [unknown, string];
-
-interface JsxComponentCustomProps {
-  vShow?: boolean;
-  vSlots?: VueSlots;
-  vModel?: VueModel;
-  vModels?: VueModel[];
-  vHtml?: string | JSX.Element | null;
-}
-
-declare module 'vue' {
-  interface HTMLAttributes extends JsxComponentCustomProps {}
-
-  interface ComponentCustomProps extends HTMLAttributes {}
+  const component: DefineComponent<Record<string, never>, Record<string, never>, any>;
+  export default component;
 }
